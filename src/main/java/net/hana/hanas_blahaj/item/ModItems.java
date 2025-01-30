50e6ce0f76313d228d0d0477d113657090d0cce8
ThievishJoke <hanayokoikimo@gmail.com>
2025-01-30 02:13:33 -0500

    model json fixes

--- src/main/java/net/hana/hanas_blahaj/item/ModItems.java
+++ src/main/java/net/hana/hanas_blahaj/item/ModItems.java
@@ -9,22 +9,22 @@ import net.minecraft.util.Rarity;
 
 public class ModItems {
 
-    public static final Item WHITE_BLAHAJ = registerItem("white_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.EPIC)));
-    public static final Item ORANGE_BLAHAJ = registerItem("orange_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.EPIC)));
-    public static final Item MAGENTA_BLAHAJ = registerItem("magenta_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.EPIC)));
-    public static final Item LIGHT_BLUE_BLAHAJ = registerItem("light_blue_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.EPIC)));
-    public static final Item YELLOW_BLAHAJ = registerItem("yellow_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.EPIC)));
-    public static final Item LIME_BLAHAJ = registerItem("lime_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.EPIC)));
-    public static final Item PINK_BLAHAJ = registerItem("pink_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.EPIC)));
-    public static final Item GRAY_BLAHAJ = registerItem("gray_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.EPIC)));
-    public static final Item LIGHT_GRAY_BLAHAJ = registerItem("light_gray_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.EPIC)));
-    public static final Item CYAN_BLAHAJ = registerItem("cyan_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.EPIC)));
-    public static final Item PURPLE_BLAHAJ = registerItem("purple_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.EPIC)));
-    public static final Item BLAHAJ = registerItem("blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.EPIC)));
-    public static final Item BROWN_BLAHAJ = registerItem("brown_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.EPIC)));
-    public static final Item GREEN_BLAHAJ = registerItem("green_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.EPIC)));
-    public static final Item RED_BLAHAJ = registerItem("red_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.EPIC)));
-    public static final Item BLACK_BLAHAJ = registerItem("black_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.EPIC)));
+    public static final Item WHITE_BLAHAJ = registerItem("white_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.COMMON)));
+    public static final Item ORANGE_BLAHAJ = registerItem("orange_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.COMMON)));
+    public static final Item MAGENTA_BLAHAJ = registerItem("magenta_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.COMMON)));
+    public static final Item LIGHT_BLUE_BLAHAJ = registerItem("light_blue_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.COMMON)));
+    public static final Item YELLOW_BLAHAJ = registerItem("yellow_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.COMMON)));
+    public static final Item LIME_BLAHAJ = registerItem("lime_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.COMMON)));
+    public static final Item PINK_BLAHAJ = registerItem("pink_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.COMMON)));
+    public static final Item GRAY_BLAHAJ = registerItem("gray_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.COMMON)));
+    public static final Item LIGHT_GRAY_BLAHAJ = registerItem("light_gray_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.COMMON)));
+    public static final Item CYAN_BLAHAJ = registerItem("cyan_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.COMMON)));
+    public static final Item PURPLE_BLAHAJ = registerItem("purple_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.COMMON)));
+    public static final Item BLAHAJ = registerItem("blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.COMMON)));
+    public static final Item BROWN_BLAHAJ = registerItem("brown_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.COMMON)));
+    public static final Item GREEN_BLAHAJ = registerItem("green_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.COMMON)));
+    public static final Item RED_BLAHAJ = registerItem("red_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.COMMON)));
+    public static final Item BLACK_BLAHAJ = registerItem("black_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.COMMON)));
 
     //neon blahaj
     public static final Item NEON_PINK_BLAHAJ = registerItem("neon_pink_blahaj", new Item(new Item.Settings().maxCount(1).rarity(Rarity.EPIC)));
